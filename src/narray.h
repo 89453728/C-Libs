cd70d7fb01232bd1b5684b3bada947132dca0625
computer <89453728>
2021-12-18 10:27:09 +0100

    morning: pushing to other computer

--- src/narray.h
+++ src/narray.h
@@ -1,4 +1,124 @@
 #ifndef NARRAY_H
 #define NARRAY_H
 
+#include <iostream>
+
+typedef int narray_len; 
+
+namespace ff {	
+	
+	template <class T>
+	class NArray {
+		private: 
+			T *a;
+			narray_len size;
+		public: 
+			NArray();
+			NArray(const narray_len s);
+			NArray(const T *e,const narray_len tam);
+			
+			NArray operator +(const NArray &a) const;
+			NArray operator +(const T a) const;
+			NArray operator -(const NArray &a) const;
+			NArray operator -(const T a) const;
+			NArray operator *(const NArray &a) const;
+			NArray operator *(const T a) const;
+			NArray operator /(const T a) const;
+			T& operator [](const narray_len p);
+			narray_len len()const;
+	};
+}
+
+template<class T>
+ff::NArray<T>::NArray() {
+	*this.size = 0;
+}
+template<class T>
+ff::NArray<T>::NArray(const narray_len s){
+	this->a = new T[s];
+	this->size = s;
+}
+template<class T>
+ff::NArray<T>::NArray(const T *e,const narray_len tam){
+	*this.m = new T[tam];
+}
+
+template<class T>
+ff::NArray<T> ff::NArray<T>::operator +(const ff::NArray<T> &a)const{
+	if(*this.len() != a.len())
+		return ff::NArray<T>();
+		
+	ff::NArray<T> ret(*this.len());
+	for(narray_len i=0;i<*this.len();i++){
+		ret[i] = *this[i] + a[i];
+	}
+	return ret;
+}
+template<class T>
+ff::NArray<T> ff::NArray<T>::operator +(const T a)const{
+	ff::NArray<T> ret(*this.len());
+	for(narray_len i=0;i<*this.len();i++){
+		ret[i] = *this[i]+a;
+	}
+	return ret;
+}
+template<class T>
+ff::NArray<T> ff::NArray<T>::operator -(const ff::NArray<T> &a)const{
+	if(*this.len() != a.len())
+		return ff::NArray<T>();
+	
+	ff::NArray<T> ret(*this.len());
+	for(narray_len i=0;i<*this.len();i++){
+		ret[i] = *this[i] - a[i];
+	}
+	return ret;
+}	
+template<class T>
+ff::NArray<T> ff::NArray<T>::operator -(const T a)const{
+	ff::NArray<T> ret(*this.len());
+	for(narray_len i=0;i<*this.len();i++){
+		ret[i] = *this[i]-a;
+	}
+	return ret;
+}
+template<class T>
+ff::NArray<T> ff::NArray<T>::operator *(const ff::NArray<T> &a)const{
+	if(*this.len() != a.len())
+		return ff::NArray<T>();
+	
+	ff::NArray<T> ret(*this.len());
+	for(narray_len i=0;i<*this.len();i++){
+		ret[i] = *this[i] * a[i];
+	}
+	return ret;
+}
+template<class T>
+ff::NArray<T> ff::NArray<T>::operator *(const T a)const {
+	ff::NArray<T> ret(*this.len());
+	for(narray_len i=0;i<*this.len();i++){
+		ret[i] = *this[i]*a;
+	}
+	return ret;
+}
+template<class T>
+ff::NArray<T> ff::NArray<T>::operator /(const T a)const {
+	ff::NArray<T> ret(*this.len());
+	for(narray_len i=0;i<*this.len();i++){
+		ret[i] = *this[i]/a;
+	}
+	return ret;
+}
+template<class T>
+T& ff::NArray<T>::operator [](const narray_len p) {
+	if(p < *this.len())
+		return *this->a[p];
+	
+	std::cerr << "NArray error: position must be less than vector\n"; 
+}
+template<class T>
+
+narray_len ff::NArray<T>::len()const{
+	return *this.size;
+}
+
 #endif
--- test/main.cc
+++ test/main.cc
@@ -1,4 +1,7 @@
 #include "cmp.h"
+#include "narray.h"
+#include <iostream>
+
 
 using namespace ff;
 
@@ -7,5 +10,14 @@ int main(){
 	doubleComplex b(4.55,-7.84);
 	a += a*b;
 	a.c_print();
+
+	ff::NArray<int> arr(6);
+	for(narray_len i=0;i<6;i++)
+		arr[i] = i+1;
+	
+
+	for(narray_len i=0;i<6;i++)
+		std::cout << "arr[" << i << "] = " << arr[i] << "\n";
+	
 	return 0;
 }
